32b6a848f65bc54b6bb4b95e8216e1efc3882dd2
nit <akshay.gurnule@gmail.com>
2018-07-27 11:09:52 +0530

    full

--- app/src/main/java/com/akshay/mvpincomeapp/model/IncExpModel.kt
+++ app/src/main/java/com/akshay/mvpincomeapp/model/IncExpModel.kt
@@ -1,18 +1,40 @@
 package com.akshay.mvpincomeapp.model
 
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
 import com.akshay.mvpincomeapp.beans.IncExpBean
 import com.akshay.mvpincomeapp.presenter.IncExpPresenterAPI
 import com.akshay.mvpincomeapp.view.IncExpView
+import com.akshay.mvpincomeapp.view.MainActivity
 
 class IncExpModel: IncExpPresenterAPI {
+    var api : MainActivity? = null
+    var db : SQLiteDatabase? = null
     constructor(view: IncExpView){
+        api = MainActivity()
+        db = api!!.openOrCreateDatabase("incexp",Context.MODE_PRIVATE,null)
+        db!!.execSQL("create table if not exists incexp(_id integer primary key autoincrement, date_ varchar(50), money integer,desc_ varchar(500), type varchar(50))")
 
     }
     override fun addInput(bean: IncExpBean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var cv = ContentValues()
+        cv.put("date_",bean.date)
+        cv.put("money",bean.money)
+        cv.put("desc_",bean.desc)
+        cv.put("type",bean.type)
+
+        var status = db!!.insert("incexp",null,cv)
+        if(status==-1.toLong()) {
+            api!!.addResponse("Failed")
+        }
+        else{
+            api!!.addResponse("Successfull")
+        }
     }
 
     override fun readInput(type: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var cursor = db!!.query("incexp",null,"type =?", arrayOf(type),null,null,null)
+        api!!.readResponse(cursor)
     }
 }
--- app/src/main/java/com/akshay/mvpincomeapp/view/MainActivity.kt
+++ app/src/main/java/com/akshay/mvpincomeapp/view/MainActivity.kt
@@ -1,21 +1,41 @@
 package com.akshay.mvpincomeapp.view
 
+import android.content.ContentValues
+import android.database.Cursor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SimpleCursorAdapter
+import android.widget.Toast
 import com.akshay.mvpincomeapp.R
+import com.akshay.mvpincomeapp.beans.IncExpBean
+import com.akshay.mvpincomeapp.model.IncExpModel
+import com.akshay.mvpincomeapp.presenter.IncExpPresenterAPI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(),IncExpView {
-    override fun addResponse() {
-
+    override fun addResponse(msg: String) {
+        Toast.makeText(this,msg,Toast.LENGTH_LONG).show()
     }
 
-    override fun readResponse() {
-
+    override fun readResponse(c: Cursor) {
+        var mya = SimpleCursorAdapter(this@MainActivity,R.layout.iview,c, arrayOf("date_","money","desc_","type"), intArrayOf(R.id.tv1,R.id.tv2,R.id.tv3,R.id.tv4),0)
+        lview.adapter = mya
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        add.setOnClickListener{
+            var api = IncExpModel(this@MainActivity)
+            var bean = IncExpBean(et1.text.toString().toInt(),et2.text.toString(),et3.text.toString(),sp1.selectedItem.toString())
+            api.addInput(bean)
+        }
+
+        read.setOnClickListener {
+            var api = IncExpModel(this@MainActivity)
+            api.readInput(sp1.selectedItem.toString())
+        }
 
     }
 }
